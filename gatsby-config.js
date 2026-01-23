ade267c7f1ca81c22a1d02c008b58bcc3e5293b3
Joe Heffernan <interim17@gmail.com>
2026-01-23 14:41:47 -0800

    use link directive and slugs to map foreign key relationships

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,4 @@ module.exports = {
         }, // must be after other CSS plugins
         "gatsby-plugin-netlify", // make sure to keep it last in the array
     ],
-    mapping: {
-        "MarkdownRemark.frontmatter.dataset": `MarkdownRemark.frontmatter.name`,
-    },
 };
