8411c40a32c844a76ee48922f144e3245e683bae
Joe Heffernan <interim17@gmail.com>
2026-03-10 23:30:18 -0700

    remove extra layout

--- src/pages/resource/index.tsx
+++ src/pages/resource/index.tsx
@@ -3,41 +3,33 @@ import { Helmet } from "react-helmet-async";
 
 import { Link, PageProps, graphql } from "gatsby";
 
-import Layout from "../../components/Layout";
-
 const ResourcesPage: React.FC<PageProps<Queries.ResourcesIndexQueryQuery>> = ({
     data,
 }) => {
     const { allResource, site } = data;
 
     if (!allResource || !site) {
-        return (
-            <Layout>
-                <p>Data not found.</p>
-            </Layout>
-        );
+        return <p>Data not found.</p>;
     }
 
     const { nodes } = allResource;
     const title = site.siteMetadata?.title || "Title";
     return (
-        <Layout>
-            <section>
-                <Helmet title={`Resources | ${title}`} />
-                <div>
-                    <h1>Resources</h1>
-                    <ul>
-                        {nodes.map((node) => (
-                            <li key={node.slug}>
-                                <Link to={node.slug}>{node.name}</Link>
-                                {node.description && <p>{node.description}</p>}
-                                {node.type && <p>{node.type}</p>}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </section>
-        </Layout>
+        <section>
+            <Helmet title={`Resources | ${title}`} />
+            <div>
+                <h1>Resources</h1>
+                <ul>
+                    {nodes.map((node) => (
+                        <li key={node.slug}>
+                            <Link to={node.slug}>{node.name}</Link>
+                            {node.description && <p>{node.description}</p>}
+                            {node.type && <p>{node.type}</p>}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
     );
 };
 
