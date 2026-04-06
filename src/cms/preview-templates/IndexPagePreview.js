0b762426a9bbc43d7d252b8b102f2e8d1b1cf363
meganrm <meganrm@gmail.com>
2026-04-06 15:29:40 -0700

    make sure cms previews work

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -2,27 +2,31 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-import { IndexPageTemplate } from "../../templates/index-page";
-
 const IndexPagePreview = ({ entry, getAsset }) => {
     const data = entry.getIn(["data"]).toJS();
+    if (!data) return <div>Loading...</div>;
 
-    if (data) {
-        return (
-            <IndexPageTemplate
-                image={getAsset(data.image)}
-                title={data.title}
-            />
-        );
-    } else {
-        return <div>Loading...</div>;
-    }
+    return (
+        <div>
+            <header>
+                <span>{data.title || "Browse Ideas"}</span>
+            </header>
+            {data.image && (
+                <img
+                    src={getAsset(data.image)}
+                    alt=""
+                    style={{ maxWidth: "100%" }}
+                />
+            )}
+            <p style={{ padding: "1rem", color: "#888" }}>
+                (Idea list is populated from content at build time)
+            </p>
+        </div>
+    );
 };
 
 IndexPagePreview.propTypes = {
-    entry: PropTypes.shape({
-        getIn: PropTypes.func,
-    }),
+    entry: PropTypes.shape({ getIn: PropTypes.func }),
     getAsset: PropTypes.func,
 };
 
