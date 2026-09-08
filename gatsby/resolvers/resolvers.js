c881ce4fe7e59e48ad4e4e15f9bbf9187a27821f
Joe Heffernan <interim17@gmail.com>
2026-09-08 09:25:38 -0700

    add resource intro to idea data model

--- gatsby/resolvers/resolvers.js
+++ gatsby/resolvers/resolvers.js
@@ -44,6 +44,9 @@ const createIdeaPostResolver = (reporter) => ({
     nextSteps: {
         resolve: (source) => source.nextSteps ?? null,
     },
+    resourcesIntro: {
+        resolve: (source) => source.resourcesIntro ?? null,
+    },
     relatedIdeas: {
         resolve: async (source, _args, context) => {
             const names = resolveToArray(source.related_ideas);
